b09baa98280acd43bc9ceb08c43582984efafa6b
Taylor Holliday <taylor@audulus.com>
2022-02-14 07:27:08 -0800

    Update MSLViewTests.swift

--- Tests/MSLViewTests/MSLViewTests.swift
+++ Tests/MSLViewTests/MSLViewTests.swift
@@ -3,9 +3,5 @@ import XCTest
 
 final class MSLViewTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MSLView().text, "Hello, World!")
     }
 }
