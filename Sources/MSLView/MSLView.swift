9bb867af1bbc3173db0fb802b3083ede57a849ce
Taylor Holliday <taylor@audulus.com>
2022-02-14 12:35:17 -0800

    Don't constantly redraw

--- Sources/MSLView/MSLView.swift
+++ Sources/MSLView/MSLView.swift
@@ -31,7 +31,8 @@ public struct MSLView<T> : NSViewRepresentable {
     public func makeNSView(context: Context) -> some NSView {
         let metalView = MTKView(frame: CGRect(x: 0, y: 0, width: 1024, height: 768),
                                 device: MTLCreateSystemDefaultDevice()!)
-        metalView.clearColor = MTLClearColorMake(0.0, 0.0, 0.0, 1.0)
+        metalView.enableSetNeedsDisplay = true
+        metalView.isPaused = true
         metalView.delegate = context.coordinator.renderer
         context.coordinator.renderer.setShader(source: shader)
         return metalView
@@ -40,6 +41,7 @@ public struct MSLView<T> : NSViewRepresentable {
     public func updateNSView(_ nsView: NSViewType, context: Context) {
         context.coordinator.renderer.setShader(source: shader)
         context.coordinator.renderer.constants = constants
+        nsView.setNeedsDisplay(nsView.bounds)
     }
 }
 #else
@@ -72,14 +74,17 @@ public struct MSLView<T> : UIViewRepresentable {
     public func makeUIView(context: Context) -> some UIView {
         let metalView = MTKView(frame: CGRect(x: 0, y: 0, width: 1024, height: 768),
                                 device: MTLCreateSystemDefaultDevice()!)
-        metalView.clearColor = MTLClearColorMake(0.0, 0.0, 0.0, 1.0)
+        metalView.enableSetNeedsDisplay = true
+        metalView.isPaused = true
         metalView.delegate = context.coordinator.renderer
         context.coordinator.renderer.setShader(source: shader)
         return metalView
     }
 
-    public func updateUIView(_ nsView: UIViewType, context: Context) {
+    public func updateUIView(_ uiView: UIViewType, context: Context) {
         context.coordinator.renderer.setShader(source: shader)
+        context.coordinator.renderer.constants = constants
+        uiView.setNeedsDisplay()
     }
 
 }
