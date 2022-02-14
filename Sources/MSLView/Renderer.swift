0189a01ad56f4cc92abae2c5fdc656d15e12ea7e
Taylor Holliday <taylor@audulus.com>
2022-02-14 07:27:13 -0800

    Create Renderer.swift

---
+++ Sources/MSLView/Renderer.swift
@@ -0,0 +1,54 @@
+import Foundation
+import MetalKit
+
+class Renderer: NSObject, MTKViewDelegate {
+
+    var device: MTLDevice!
+    var queue: MTLCommandQueue!
+
+    static let MaxBuffers = 3
+    private let inflightSemaphore = DispatchSemaphore(value: MaxBuffers)
+
+    init(device: MTLDevice) {
+        self.device = device
+        queue = device.makeCommandQueue()
+
+    }
+
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+
+    }
+
+    func draw(in view: MTKView) {
+
+        let size = view.frame.size
+        let w = Float(size.width)
+        let h = Float(size.height)
+        // let scale = Float(view.contentScaleFactor)
+
+        if w == 0 || h == 0 {
+            return
+        }
+
+        // use semaphore to encode 3 frames ahead
+        _ = inflightSemaphore.wait(timeout: DispatchTime.distantFuture)
+
+        let commandBuffer = queue.makeCommandBuffer()!
+
+        let semaphore = inflightSemaphore
+        commandBuffer.addCompletedHandler { _ in
+            semaphore.signal()
+        }
+
+
+        if let renderPassDescriptor = view.currentRenderPassDescriptor, let currentDrawable = view.currentDrawable {
+
+            renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0, 0, 0, 1)
+
+
+            commandBuffer.present(currentDrawable)
+        }
+        commandBuffer.commit()
+
+    }
+}
